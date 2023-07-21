668ee83f09888f7f29e0ee0bbce3781052eaa112
Romain Roffé <rofferom@gmail.com>
2023-07-21 11:09:07 +0200

    Extract the generic control code in a dedicated file

---
+++ src/include/libtxproto/control.h
@@ -0,0 +1,26 @@
+/*
+ * This file is part of txproto.
+ *
+ * txproto is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * txproto is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with txproto; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <libtxproto/txproto_main.h>
+
+ctrl_fn get_ctrl_fn(void *ctx);
+
+int generic_ctrl(TXMainContext *ctx,
+                 AVBufferRef *ref,
+                 SPEventType flags,
+                 void *arg);
