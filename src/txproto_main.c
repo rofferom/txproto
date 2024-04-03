rofferom/txproto

commit 8f8a8cf27579201707321d14ea3f6bd98255afd5

Test coroutine entrypoint

src/txproto_main.c
@@ -325,13 +325,19 @@ int main(int argc, char *argv[])
 
     /* Run script entrypoint if specified */
     if (script_entrypoint && strlen(script_entrypoint)) {
-        lua_State *L = sp_lua_lock_interface(ctx->lua);
+        TXLuaContext *lctx = sp_lua_create_thread(ctx->lua, ctx);
+        if (!lctx) {
+            err = AVERROR(ENOMEM);
+            goto end;
+        }
+
+        lua_State *L = lctx->L;
 
         lua_getglobal(L, script_entrypoint);
         if (!lua_isfunction(L, -1)) {
             sp_log(ctx, SP_LOG_ERROR, "Entrypoint \"%s\" not found!\n",
                    script_entrypoint);
-            err = sp_lua_unlock_interface(ctx->lua, AVERROR(ENOENT));
+            err = AVERROR(ENOENT);
             goto end;
         }
 
@@ -339,17 +345,14 @@ int main(int argc, char *argv[])
         for(; optind < argc; optind++)
             lua_pushstring(L, argv[optind]);
 
-        if ((err = sp_lua_run_generic_yieldable(ctx->lua, args, 1, 1)) < 0)
+        if ((err = sp_lua_run_generic_yieldable(lctx, args, 1, 1)) < 0)
             goto end;
 
-        sp_lua_unlock_interface(ctx->lua, 0);
+        sp_lua_close_ctx(&lctx);
+    } else {
+        while (1)
+            av_usleep(UINT_MAX);
     }
-
-    /* We weren't told to exit or anything, so...
-     * In the future, maybe we should put an event loop here to process
-     * periodic events */
-    while (1)
-        av_usleep(UINT_MAX);
 
 end:
     av_freep(&lua_libs_list);

DOCS/examples/audio_rec.lua
@@ -11,34 +11,40 @@ function muxer_stats(stats)
     tx.set_status(statusline)
 end
 
+-- Setup
+event = tx.register_io_cb(io_update_cb)
+event.destroy()
+
+tx.set_epoch(0)
+
+source_mic = tx.create_io(audio_mic_id, {
+        buffer_ms = 8,
+    })
+
+encoder_a = tx.create_encoder({
+        encoder = "libopus",
+        options = {
+            b = 10^3 --[[ Kbps ]] * 256,
+            application = "audio",
+            frame_duration = 20,
+            vbr = "on",
+        },
+    })
+encoder_a.link(source_mic)
+
+muxer_a = tx.create_muxer({
+        out_url = "rec.opus",
+        priv_options = { dump_info = true, low_latency = true },
+    })
+muxer_a.link(encoder_a)
+muxer_a.schedule("stats", muxer_stats)
+
+tx.commit()
+
 function main(...)
-    event = tx.register_io_cb(io_update_cb)
-    event.destroy()
-
-    tx.set_epoch(0)
-
-    source_mic = tx.create_io(audio_mic_id, {
-            buffer_ms = 8,
-        })
-
-    encoder_a = tx.create_encoder({
-            encoder = "libopus",
-            options = {
-                b = 10^3 --[[ Kbps ]] * 256,
-                application = "audio",
-                frame_duration = 120,
-                vbr = "on",
-            },
-        })
-    encoder_a.link(source_mic)
-
-    muxer_a = tx.create_muxer({
-            out_url = "rec.opus",
-            priv_options = { dump_info = true, low_latency = false },
-        })
-    muxer_a.link(encoder_a)
-    muxer_a.schedule("stats", muxer_stats)
-
-    tx.commit()
-
+    while (true)
+    do
+        coroutine.yield()
+        print("Test")
+    end
 end

src/lua_common.c
@@ -923,16 +923,18 @@ int sp_lua_run_generic_yieldable(TXLuaContext *lctx, int nb_args, int clean_stac
             /* Unlock */
             sp_lua_unlock_interface(lctx, 0);
 
-            char *fstr = sp_event_flags_to_str_buf(event_await);
-            sp_log(lctx, SP_LOG_DEBUG, "Thread waiting on an event signal (id:%lu %s)...\n",
-                   sp_event_get_id(event_await), fstr);
-            av_free(fstr);
+            if (event_await) {
+                char *fstr = sp_event_flags_to_str_buf(event_await);
+                sp_log(lctx, SP_LOG_DEBUG, "Thread waiting on an event signal (id:%lu %s)...\n",
+                       sp_event_get_id(event_await), fstr);
+                av_free(fstr);
 
-            /* Await event, if any */
-            sp_event_unref_await(&event_await);
+                /* Await event, if any */
+                sp_event_unref_await(&event_await);
 
-            sp_log(lctx, SP_LOG_DEBUG, "Event signalled, %s\n",
-                   ret == LUA_YIELD ? "resuming!" : "exiting.");
+                sp_log(lctx, SP_LOG_DEBUG, "Event signalled, %s\n",
+                       ret == LUA_YIELD ? "resuming!" : "exiting.");
+            }
 
             /* Lock again */
             sp_lua_lock_interface(lctx);

commit abce6390d497f760d8d05e9a6df83890418c6b2a

lua: Restore previous entrypoint behavior
Regression introduced by 8f8a8cf27579201707321d14ea3f6bd98255afd5

src/txproto_main.c
@@ -361,10 +361,13 @@ int main(int argc, char *argv[])
 
         sp_lua_unlock_interface(lctx, 0);
         sp_lua_close_ctx(&lctx);
-    } else {
-        while (1)
-            av_usleep(UINT_MAX);
     }
+
+    /* We weren't told to exit or anything, so...
+     * In the future, maybe we should put an event loop here to process
+     * periodic events */
+    while (1)
+        av_usleep(UINT_MAX);
 
 end:
     av_freep(&lua_libs_list);